aa8cfdcbced0e61bd044a01884134bfb4e086758
Matt Lacey <matt@mrlacey.co.uk>
2018-04-08 21:11:17 +0100

    Add ability to copy to clipboard
    For #2

--- RapidXAML.VSIX/RapidXamlToolkit/Commands/CopyToClipboardCommand.cs
+++ RapidXAML.VSIX/RapidXamlToolkit/Commands/CopyToClipboardCommand.cs
@@ -4,41 +4,20 @@
 
 using System;
 using System.ComponentModel.Design;
-using System.Globalization;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
 
 namespace RapidXamlToolkit
 {
-    /// <summary>
-    /// Command handler
-    /// </summary>
-    internal sealed class CopyToClipboardCommand
+    internal sealed class CopyToClipboardCommand : GetXamlFromCodeWindowBaseCommand
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
         public const int CommandId = 4128;
 
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
         public static readonly Guid CommandSet = new Guid("8c20aab1-50b0-4523-8d9d-24d512fa8154");
 
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
         private readonly AsyncPackage package;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CopyToClipboardCommand"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
         private CopyToClipboardCommand(AsyncPackage package, OleMenuCommandService commandService)
         {
             this.package = package ?? throw new ArgumentNullException(nameof(package));
@@ -49,18 +28,12 @@ namespace RapidXamlToolkit
             commandService.AddCommand(menuItem);
         }
 
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
         public static CopyToClipboardCommand Instance
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
         private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
         {
             get
@@ -69,42 +42,20 @@ namespace RapidXamlToolkit
             }
         }
 
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Verify the current thread is the UI thread - the call to AddCommand in CreateXamlStringCommand's constructor requires
-            // the UI thread.
+            // Verify the current thread is the UI thread - the call to AddCommand in CreateXamlStringCommand's constructor requires the UI thread.
             ThreadHelper.ThrowIfNotOnUIThread();
 
             OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new CopyToClipboardCommand(package, commandService);
+
+            AnalyzerBase.ServiceProvider = (IServiceProvider)Instance.ServiceProvider;
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e)
         {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "CopyToClipboardCommand";
-
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            this.GetXaml(Instance.ServiceProvider, (msg) => { Clipboard.SetText(msg); });
         }
     }
 }
---
+++ RapidXAML.VSIX/RapidXamlToolkit/Commands/GetXamlFromCodeWindowBaseCommand.cs
@@ -0,0 +1,122 @@
+﻿// <copyright file="GetXamlFromCodeWindowBaseCommand.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace RapidXamlToolkit
+{
+    internal class GetXamlFromCodeWindowBaseCommand
+    {
+        public void GetXaml(Microsoft.VisualStudio.Shell.IAsyncServiceProvider serviceProvider, Action<string> ifContent)
+        {
+            if (AnalyzerBase.GetSettings().Profiles.Any())
+            {
+                var dte = (EnvDTE.DTE)serviceProvider.GetServiceAsync(typeof(EnvDTE.DTE)).Result;
+                var activeDocument = dte.ActiveDocument;
+
+                var textView = GetTextView(serviceProvider);
+
+                var selection = textView.Selection;
+
+                bool isSelection = selection.Start.Position != selection.End.Position;
+
+                var caretPosition = textView.Caret.Position.BufferPosition;
+
+                var document = caretPosition.Snapshot.GetOpenDocumentInCurrentContextWithChanges();
+
+                var semanticModel = document.GetSemanticModelAsync().Result;
+
+                AnalyzerOutput analyzerOutput = null;
+
+                IDocumentAnalyzer analyzer = null;
+
+                if (activeDocument.Language == "CSharp")
+                {
+                    analyzer = new CSharpAnalyzer();
+                }
+                else if (activeDocument.Language == "Basic")
+                {
+                    analyzer = new VisualBasicAnalyzer();
+                }
+
+                if (isSelection)
+                {
+                    analyzerOutput = analyzer.GetSelectionOutput(document.GetSyntaxRootAsync().Result, semanticModel, selection.Start.Position, selection.End.Position);
+                }
+                else
+                {
+                    analyzerOutput = analyzer.GetSingleItemOutput(document.GetSyntaxRootAsync().Result, semanticModel, caretPosition.Position);
+                }
+
+                if (analyzerOutput != null && analyzerOutput.OutputType != AnalyzerOutputType.None)
+                {
+                    var message = analyzerOutput.Output;
+
+                    if (!string.IsNullOrWhiteSpace(message))
+                    {
+                        ifContent?.Invoke(message);
+                    }
+                    else
+                    {
+                        // Log no output
+                    }
+
+                    ShowStatusBarMessage(serviceProvider, $"Copied XAML for {analyzerOutput.OutputType}: {analyzerOutput.Name}");
+                }
+                else
+                {
+                    // TODO [vNext]: play error noise/beep if can't do something or error?
+                    ShowStatusBarMessage(serviceProvider, "No XAML copied.");
+                }
+            }
+            else
+            {
+                ShowStatusBarMessage(serviceProvider, "No XAML copied. No profiles configured.");
+            }
+        }
+
+        private static void ShowStatusBarMessage(Microsoft.VisualStudio.Shell.IAsyncServiceProvider serviceProvider, string message)
+        {
+            try
+            {
+                ((EnvDTE.DTE)serviceProvider.GetServiceAsync(typeof(EnvDTE.DTE)).Result).StatusBar.Text = message;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static Microsoft.VisualStudio.Text.Editor.IWpfTextView GetTextView(Microsoft.VisualStudio.Shell.IAsyncServiceProvider serviceProvider)
+        {
+            var textManager = (IVsTextManager)serviceProvider.GetServiceAsync(typeof(SVsTextManager)).Result;
+
+            if (textManager == null)
+            {
+                return null;
+            }
+
+            textManager.GetActiveView(1, null, out IVsTextView textView);
+
+            if (textView == null)
+            {
+                return null;
+            }
+
+            return GetEditorAdaptersFactoryService(serviceProvider).GetWpfTextView(textView);
+        }
+
+        private static IVsEditorAdaptersFactoryService GetEditorAdaptersFactoryService(Microsoft.VisualStudio.Shell.IAsyncServiceProvider serviceProvider)
+        {
+            IComponentModel componentModel = (IComponentModel)serviceProvider.GetServiceAsync(typeof(SComponentModel)).Result;
+            return componentModel.GetService<IVsEditorAdaptersFactoryService>();
+        }
+    }
+}
