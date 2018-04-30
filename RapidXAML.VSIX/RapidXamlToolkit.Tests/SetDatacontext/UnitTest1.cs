bf2e13348918847de4addd4faabc31a28405e17d
Matt Lacey <matt@mrlacey.co.uk>
2018-04-30 14:12:28 +0100

    adding notes for more required tests

---
+++ RapidXAML.VSIX/RapidXamlToolkit.Tests/SetDatacontext/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RapidXamlToolkit.Tests.SetDatacontext
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // TODO: TEST - has xaml detected
+            // TODO: TEST - adding to xaml
+            // TODO: TEST - VB detect in CB ctor
+            // TODO: TEST - VB detect in CB outside ctor
+            // TODO: TEST - VB add to CB ctor
+            // TODO: TEST - VB add to CB outside ctor
+            // TODO: TEST - VB add to CB ctor when ctor does not exist
+            // TODO: TEST - VB add to CB in ctor and outside ctor
+            // TODO: TEST - VB add to CB in ctor and outside ctor when ctor does not exist
+            // TODO: TEST - VB add to cb outside ctor when no ctor exists & not adding to ctor
+            // TODO: TEST - C# detect in CB ctor
+            // TODO: TEST - C# detect in CB outside ctor
+            // TODO: TEST - C# add to CB ctor
+            // TODO: TEST - C# add to CB outside ctor
+            // TODO: TEST - C# add to CB ctor when ctor does not exist
+            // TODO: TEST - C# add to CB in ctor and outside ctor
+            // TODO: TEST - C# add to CB in ctor and outside ctor when ctor does not exist
+            // TODO: TEST - C# add to cb outside ctor when no ctor exists & not adding to ctor
+        }
+    }
+}
