533958fc482e19462565a2fb85a7f5e595897d4f
Matt Lacey <matt@mrlacey.co.uk>
2018-05-24 22:35:45 +0100

    Added localized versions of each StringRes.resx file to send for localization
    #7

--- RapidXAML.VSIX/RapidXamlToolkit/Properties/AssemblyInfo.cs
+++ RapidXAML.VSIX/RapidXamlToolkit/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
