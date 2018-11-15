6a2dabff4027ec12d809b92cc19d111a32781454
Matt Lacey <matt@mrlacey.co.uk>
2018-11-15 21:21:08 +0000

    Add syntax highlighting in Options windows
    XAML, C# & VB.Net
    Includes handling HighContrast of the syntax highlighitng
    For #16

--- RapidXAML.VSIX/OptionsEmulator/MainWindow.xaml.cs
+++ RapidXAML.VSIX/OptionsEmulator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+using System;
 using System.Linq;
 using System.Windows;
 using RapidXamlToolkit.Options;
@@ -16,7 +17,8 @@ namespace OptionsEmulator
             this.Settings.DataContext = ConfiguredSettings.GetDefaultSettings();
             this.Settings.DisableButtonsForEmulator();
 
-            this.ProfileConfig.DataContext = ConfiguredSettings.GetDefaultSettings().Profiles.First();
+            // Pick a random profile to show in tab - makes repeated testing more interesting
+            this.ProfileConfig.SetDataContextAndHost(ConfiguredSettings.GetDefaultSettings().Profiles.OrderBy(p => Guid.NewGuid()).First(), null);
             this.ProfileConfig.DisableButtonsForEmulator();
         }
     }
