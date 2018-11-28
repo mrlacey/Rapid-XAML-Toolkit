5c8c3175524a5789b32a66f8752aecd0444441fb
sibille <sibille@ilitia.com>
2018-11-28 08:38:06 +0100

    Added script to set identity and version. Changed command names to include environment to distinguish installed version

---
+++ _build/Extension-SetIdentityAndVersion.ps1
@@ -0,0 +1,208 @@
+# THIS SCRITP WILL NEED TO BE SPLITED NEXT TIME, GETTING TO LONG.
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$vsixManifestFile,
+
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$vsixIdentity,
+
+  [Parameter(Mandatory=$True,Position=3)]
+  [string]$vsixDisplayName,
+
+  [Parameter(Mandatory=$True,Position=4)]
+  [string]$buildNumber,
+
+  [Parameter(Mandatory=$True,Position=5)]
+  [string]$vsixCommandMenuName,
+
+  [Parameter(Mandatory=$True,Position=6)]
+  [string]$packageGuid,
+
+  [Parameter(Mandatory=$True,Position=7)]
+  [string]$cmdSetGuid,
+
+  [Parameter(Mandatory=$False,Position=8)]
+  [string]$targetVsixCommandMenuName = "Rapid XAML (local)",
+
+  [Parameter(Mandatory=$False,Position=9)]
+  [string]$targetPackageGuid = "c735dfc3-c416-4501-bc33-558e2aaad8c5",
+
+  [Parameter(Mandatory=$False,Position=10)]
+  [string]$targetCmdSetGuid = "8c20aab1-50b0-4523-8d9d-24d512fa8154"
+)
+
+# TODO
+$vsixManifestFile = "RapidXAML.VSIX/RapidXamlToolkit/source.extension.vsixmanifest"
+$vsixIdentity = "RapidXamlToolkit.DevNightly.25067190-b212-4873-aecf-84547ed9b962"
+$vsixDisplayName = "Rapid Xaml Toolkit (dev-nightly)"
+$buildNumber = "1.2.3.4"
+$vsixCommandMenuName = "Rapid Xaml Toolkit (dev-nightly)"
+$packageGuid = "bd81034c-4068-47ea-9983-95e652abaeb8"
+$cmdSetGuid = "343F3EAC-C533-4120-8F96-032E50A8BA18"
+
+
+
+$VersionRegex = "(\d+)\.(\d+)\.(\d+)\.(\d+)"
+
+if($buildNumber -match $VersionRegEx){
+
+  Write-Output "Parsed Date From Build: $dateFromBuildNumber"
+
+  $revision =  [int]::Parse($matches[4]).ToString()
+  
+  $versionNumber = [int]::Parse($matches[1]).ToString() + "." + [int]::Parse($matches[2]).ToString() + "." + [int]::Parse($matches[3]).ToString() + "." + $revision
+  Write-Host "Version Number" $versionNumber
+  
+}
+else{
+	throw "Build format does not match the expected pattern (buildName_w.x.y.z)"
+}
+
+## SET IDENTITY AND VERSION IN VSIX Manifest
+if($vsixIdentity){
+  Write-Host
+  Write-Host "Setting Identity in VSIX manifest"
+  if(Test-Path($vsixManifestFile)){
+    [xml]$manifestContent = Get-Content $vsixManifestFile
+    $manifestContent.PackageManifest.Metadata.Identity.Id = $vsixIdentity
+    $manifestContent.PackageManifest.Metadata.Identity.Version = $versionNumber
+    $manifestContent.PackageManifest.Metadata.DisplayName = $vsixDisplayName
+    $resolvedPath = Resolve-Path($vsixManifestFile)
+    $manifestContent.Save($resolvedPath) 
+    Write-Host "$resolvedPath - Version, Identity & DisplayName applied ($versionNumber, $vsixIdentity, $vsixDisplayName)"
+
+    $vsixLangPacks = Get-ChildItem -include "Extension.vsixlangpack" -recurse |  Where-Object{ 
+        $_.FullName -notmatch "\\Templates\\" -and 
+        $_.FullName -notmatch "\\debug\\" -and
+        $_.FullName -notmatch "\\obj\\" -and
+        $_.FullName -match "\\RapidXamlToolkit\\"
+    }
+    if($vsixLangPacks){ 
+      Write-Host "Applying Display Name to vsixlangpack files"
+      foreach ($langPack in $vsixLangPacks) {
+        [xml]$langContent = Get-Content $langPack
+        $langContent.VsixLanguagePack.LocalizedName = $vsixDisplayName
+        $langContent.Save($langPack) 
+        Write-Host "$langPack - LocalizedName applied ($vsixDisplayName)"        
+      }
+    }
+  }
+  else{
+    throw "No VSIX manifest file found."
+  }
+}
+else{
+  throw "Identity is mandatory."
+}
+
+
+## REPLACE Command Guids
+if($cmdSetGuid -and $packageGuid){
+  Write-Host
+  Write-Host "Setting PackageGuid and CmdSetGuid in VSCT Files"
+  $vsctFiles = Get-ChildItem -include "*.vsct" -recurse |  Where-Object{ 
+      $_.FullName -notmatch "\\Templates\\" -and 
+      $_.FullName -notmatch "\\debug\\" -and
+      $_.FullName -notmatch "\\obj\\" -and
+      $_.FullName -match "\\RapidXamlToolkit\\"
+  }
+  if($vsctFiles){ 
+    Write-Host
+    Write-Host "Applying guids $cmdSetGuid, $packageGuid and command name $vsixCommandMenuName to VSCT Files"
+    foreach ($vsctFile in $vsctFiles) {
+      $vsctFileContent = Get-Content $vsctFile
+      attrib $vsctFile -r
+      $replacedVsctContent = $vsctFileContent.Replace($targetPackageGuid, $packageGuid)
+      $replacedVsctContent = $replacedVsctContent.Replace($targetCmdSetGuid, $cmdSetGuid)
+      $replacedVsctContent = $replacedVsctContent.Replace($targetVsixCommandMenuName, $vsixCommandMenuName)
+      $replacedVsctContent | Out-File $vsctFile -Encoding utf8 -Force
+      Write-Host "$vsctFile - Guids applied (PackageGuid:$packageGuid, CmdGuid:$cmdSetGuid)"        
+    }
+  }
+  else{
+    throw "No VSCT files found."
+  }
+
+  Write-Host
+  Write-Host "Applying guids $packageGuid to RapidXamlPackage.cs"
+  $path = Resolve-Path $vsixManifestFile
+  $installerPath = Split-Path $path
+  $constFile = Join-Path  $installerPath "RapidXamlPackage.cs"
+  
+  if($constFile){
+    $constFileContent = Get-Content $constFile
+    attrib $constFile -r
+    $replacedConstContent = $constFileContent.Replace($targetPackageGuid, $packageGuid)
+    $replacedConstContent | Out-File $constFile -Encoding utf8 -Force
+    Write-Host "$constFile - Guid applied (PackageGuid:$packageGuid)"
+  }
+  else{
+     throw "BaseCommand.cs constants file not found"
+  }
+
+  Write-Host
+  Write-Host "Applying guids $cmdSetGuid to BaseCommand.cs"
+  $path = Resolve-Path $vsixManifestFile
+  $installerPath = Split-Path $path
+  $constFile = Join-Path  $installerPath "Commands\BaseCommand.cs"
+  
+  if($constFile){
+    $constFileContent = Get-Content $constFile
+    attrib $constFile -r
+    $replacedConstContent = $constFileContent.Replace($targetCmdSetGuid, $cmdSetGuid)
+    $replacedConstContent | Out-File $constFile -Encoding utf8 -Force
+    Write-Host "$constFile - Guid applied (CmdGuid:$cmdSetGuid)"
+  }
+  else{
+     throw "BaseCommand.cs constants file not found"
+  }
+}
+else{
+  throw "PackageGuid and CmdSetGuid are mandatory"
+}
+
+
+## APPLY VERSION TO ASSEMBLY FILES (AssemblyVersion and AssemblyFileVersion)
+Write-Host
+Write-Host "Applying version to AssemblyInfo Files in matching the path pattern '$codePathPattern'" 
+$files = Get-ChildItem -include "*AssemblyInfo.cs" -Recurse #|  Where-Object{ $_.FullName -notmatch "\\Templates\\" }
+if($files)
+{
+    Write-Host "Will apply $versionNumber to $($files.count) files."
+
+    $assemblyVersionRegEx = "\(""$VersionRegex""\)" 
+    $assemblyVersionReplacement = "(""$versionNumber"")"
+
+    foreach ($file in $files) {
+        $filecontent = Get-Content($file)
+        attrib $file -r
+        $filecontent -replace $assemblyVersionRegEx, $assemblyVersionReplacement | Out-File $file utf8
+        Write-Host "$file - version applied"
+    }
+}
+else
+{
+    Write-Warning "No files found to apply version."
+}
+
+
+## APPLY VERSION RelayCommandPackage
+Write-Host
+Write-Host "Applying version to RapidXamlPackage.cs"
+$files = Get-ChildItem -include "RapidXamlPackage.cs" -Recurse #|  Where-Object{ $_.FullName -notmatch "\\Templates\\" }
+if($files)
+{
+    Write-Host "Will apply $versionNumber to $($files.count) files."
+
+    foreach ($file in $files) {
+        $filecontent = Get-Content($file)
+        attrib $file -r
+        $filecontent -replace $VersionRegex, $versionNumber | Out-File $file utf8
+        Write-Host "$file - version applied"
+    }
+}
+else
+{
+    Write-Warning "No relaycommandPackage found to apply version."
+}
