8d467ca44391a172f68f6f0bfecb16e999c33179
Matt Lacey <matt@mrlacey.co.uk>
2018-12-20 11:14:23 +0000

    Add validation warnings to TextBoxes in options UI
    For #16

--- RapidXAML.VSIX/OptionsEmulator/App.xaml.cs
+++ RapidXAML.VSIX/OptionsEmulator/App.xaml.cs
@@ -1,11 +1,37 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace OptionsEmulator
 {
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (e.Args.Any())
+            {
+                try
+                {
+                    if (!string.IsNullOrWhiteSpace(e.Args[0]))
+                    {
+                        var culture = new CultureInfo(e.Args[0]);
+
+                        Thread.CurrentThread.CurrentCulture = culture;
+                        Thread.CurrentThread.CurrentUICulture = culture;
+                    }
+                }
+                catch (System.Exception exc)
+                {
+                    Debug.WriteLine(exc);
+                }
+            }
+        }
     }
 }
