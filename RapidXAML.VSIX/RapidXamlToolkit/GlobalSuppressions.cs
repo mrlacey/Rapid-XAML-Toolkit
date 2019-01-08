171714b7a65d9a373ca3df2b25a75bdcdb34924d
Matt Lacey <matt@mrlacey.co.uk>
2019-01-08 20:56:18 +0000

    suppress overruled analyzer issues

--- RapidXAML.VSIX/RapidXamlToolkit/GlobalSuppressions.cs
+++ RapidXAML.VSIX/RapidXamlToolkit/GlobalSuppressions.cs
@@ -5,3 +5,5 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification="Documentation of code is not required.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1601:Partial elements should be documented", Justification = "Documentation of code is not required.")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1602:Enumeration items should be documented", Justification = "Documentation of code is not required.")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD010:Invoke single-threaded types on Main thread", Justification = "VSTHD109 overrules this")]
