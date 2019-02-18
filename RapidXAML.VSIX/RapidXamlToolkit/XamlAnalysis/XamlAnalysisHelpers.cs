78cdf1ce3524da029571e5bdd4145e0d275d5566
Matt Lacey <matt@mrlacey.co.uk>
2019-02-18 09:38:12 +0000

    XAH.GetAttribute

--- RapidXAML.VSIX/RapidXamlToolkit/XamlAnalysis/XamlAnalysisHelpers.cs
+++ RapidXAML.VSIX/RapidXamlToolkit/XamlAnalysis/XamlAnalysisHelpers.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace RapidXamlToolkit.XamlAnalysis
 {
-    // TODO: Merge with XamlElementExtractor?
+    // TODO: Merge with XamlElementProcessor?
     public static class XamlAnalysisHelpers
     {
         public static bool IsSelfClosing(string xaml, int startPoint = 0)
@@ -58,9 +58,28 @@ namespace RapidXamlToolkit.XamlAnalysis
 
         public static bool HasDefaultValue(string xaml, int startPoint = 0)
         {
+            var value = GetDefaultValue(xaml, startPoint);
+
+            return !string.IsNullOrEmpty(value) && value.IndexOf('<') == -1;
+        }
+
+        public static bool HasAttributeOrDefaultValue(string attributeName, string xaml, int startPoint = 0)
+        {
+            if (HasAttribute(attributeName, xaml, startPoint))
+            {
+                return true;
+            }
+            else
+            {
+                return HasDefaultValue(xaml, startPoint);
+            }
+        }
+
+        public static string GetDefaultValue(string xaml, int startPoint = 0)
+        {
             if (IsSelfClosing(xaml, startPoint))
             {
-                return false;
+                return string.Empty;
             }
             else
             {
@@ -69,7 +88,48 @@ namespace RapidXamlToolkit.XamlAnalysis
 
                 var value = xaml.Substring(openEnd + 1, closeStart - openEnd - 1);
 
-                return !string.IsNullOrEmpty(value) && value.IndexOf('<') == -1;
+                return value;
+            }
+        }
+
+        public static string GetAttribute(string attributeName, string xaml, int startPoint = 0)
+        {
+            var searchText = $"{attributeName}=\"";
+
+            var tbIndex = xaml.IndexOf(searchText, startPoint, StringComparison.Ordinal);
+
+            if (tbIndex == -1)
+            {
+                if (IsSelfClosing(xaml, startPoint))
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    var elementNameEndPos = xaml.Substring(startPoint).FirstIndexOf(" ", ">");
+                    var elementName = xaml.Substring(startPoint + 1, elementNameEndPos - 1);
+
+                    var opening = $"<{elementName}.{attributeName}>";
+                    var closing = $"</{elementName}.{attributeName}>";
+
+                    var openingPos = xaml.IndexOf(opening, elementNameEndPos, StringComparison.Ordinal);
+                    var closingPos = xaml.IndexOf(closing, elementNameEndPos, StringComparison.Ordinal);
+
+                    if (openingPos > -1 && closingPos > openingPos)
+                    {
+                        return xaml.Substring(openingPos + opening.Length, closingPos - openingPos - opening.Length);
+                    }
+                    else
+                    {
+                        return string.Empty;
+                    }
+                }
+            }
+            else
+            {
+                var tbEnd = xaml.IndexOf("\"", tbIndex + searchText.Length, StringComparison.Ordinal);
+
+                return xaml.Substring(tbIndex + searchText.Length, tbEnd - tbIndex - searchText.Length);
             }
         }
     }
