3fad74acd2aafd9996aa6c4af3afaffa766238f2
Matt Lacey <matt@mrlacey.co.uk>
2019-12-17 20:26:20 +0000

    try and fix menus not loading

--- VSIX/RapidXaml.Generation/RapidXamlGenerationPackage.cs
+++ VSIX/RapidXaml.Generation/RapidXamlGenerationPackage.cs
@@ -21,7 +21,7 @@ namespace RapidXamlToolkit
     [InstalledProductRegistration("#110", "#112", "0.0.0.0")] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(RapidXamlGenerationPackage.PackageGuidString)]
-    [ProvideOptionPage(typeof(SettingsConfigPage), "RapidXAML", "Profiles", 106, 107, true)]
+    [ProvideOptionPage(typeof(SettingsConfigPage), "Rapid XAML", "Profiles", 106, 107, true)]
     public sealed class RapidXamlGenerationPackage : AsyncPackage
     {
         public const string PackageGuidString = "ad4704fc-2e81-4406-9833-084d6642cf5b";
