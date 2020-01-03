afc28f4662a10f9e9492b8dc0ad3464e27ba20a0
Matt Lacey <matt@mrlacey.co.uk>
2020-01-03 21:41:32 +0000

    works as an exe

---
+++ VSIX/BuildAnalysisTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuildAnalysisTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the test project!");
+        }
+    }
+}
---
+++ VSIX/RapidXaml.BuildAnalysis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace RapidXamlBuildAnalysis
+{
+    public class Program 
+    {
+      //  protected override string ToolName => "RapidXaml.BuildAnalysis";
+
+        public string Command { get; set; }
+
+        static void Main(string[] args)
+        {
+           //Log.LogMessage(MessageImportance.High, "In custom tool");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Arguments: {args[0]}");
+        }
+
+        //protected override string GenerateFullPathToTool()
+        //{
+        //    return this.Command;
+        //}
+    }
+}
