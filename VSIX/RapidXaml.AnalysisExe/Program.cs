b20487a768bafb30196e99e71ac8a8f097b97d0d
Matt Lacey <matt@mrlacey.co.uk>
2020-01-04 14:02:03 +0000

    Invoke the ToolTask from nuget when compiled

---
+++ VSIX/RapidXaml.AnalysisExe/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RapidXaml.AnalysisExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("expecting a project file as first command argument.");
+            }
+            else
+            {
+                // TODO: check file exists
+                // TODO: check file is a project file
+
+                var ProjectPath = args[0];
+
+                var projFileLines = System.IO.File.ReadAllLines(ProjectPath);
+
+                foreach (var line in projFileLines)
+                {
+                    var endPos = line.IndexOf(".xaml\"");
+                    if (endPos > 1)
+                    {
+                        var startPos = line.IndexOf("Include");
+
+                        if (startPos > 1)
+                        {
+                            var relativeFilePath = line.Substring(startPos + 9, endPos + 5 - startPos - 9);
+
+                            Log.LogMessage(MessageImportance.High, $"- {relativeFilePath}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- VSIX/RapidXaml.BuildAnalysis/Program.cs
+++ VSIX/RapidXaml.BuildAnalysis/Program.cs
@@ -6,24 +6,48 @@ using System.Runtime.InteropServices;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
-namespace RapidXamlBuildAnalysis
+namespace RapidXaml.BuildAnalysis
 {
-    public class Program 
+    public class Program : ToolTask
     {
-      //  protected override string ToolName => "RapidXaml.BuildAnalysis";
+        protected override string ToolName => "RapidXaml.AnalysisExe.exe";
 
+        // This is the project file to analyze
         public string Command { get; set; }
 
         static void Main(string[] args)
         {
-           //Log.LogMessage(MessageImportance.High, "In custom tool");
             Console.WriteLine("Hello World!");
             Console.WriteLine($"Arguments: {args[0]}");
         }
 
-        //protected override string GenerateFullPathToTool()
-        //{
-        //    return this.Command;
-        //}
+        protected override string GenerateFullPathToTool()
+        {
+            // TODO: generate the full path to the exe.
+            return this.Command; // Returning the command here causes the project file to be opened.
+        }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            return this.Command;
+        }
+
+        protected override string GetWorkingDirectory()
+        {
+            // TODO: set the path to that of the project file to make references work correctly
+            return base.GetWorkingDirectory();
+        }
+
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, $"In {ToolName} tool : {Command}");
+            Log.LogWarning($"Missing {ToolName} functionality");
+            return base.Execute();
+        }
+
+        protected override int ExecuteTool(string pathToTool, string responseFileCommands, string commandLineCommands)
+        {
+            return base.ExecuteTool(pathToTool, responseFileCommands, commandLineCommands);
+        }
     }
 }
