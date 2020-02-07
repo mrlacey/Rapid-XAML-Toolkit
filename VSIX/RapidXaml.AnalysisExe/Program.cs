995cb73c6cd3978f6ed75b7e7520abf0a92bf3ce
Matt Lacey <matt@mrlacey.co.uk>
2020-02-07 09:27:01 +0000

    0.1.25 - get exe in package

--- VSIX/RapidXaml.AnalysisExe/Program.cs
+++ VSIX/RapidXaml.AnalysisExe/Program.cs
@@ -6,7 +6,7 @@ namespace RapidXaml.AnalysisExe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!  24");
+            Console.WriteLine("Hello World!  25");
 
             if (args.Length < 1)
             {
