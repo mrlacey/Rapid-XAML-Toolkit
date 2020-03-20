6a624cf3c109d8c8e161c7cb27ab6b9f979b7491
Matt Lacey <matt@mrlacey.co.uk>
2020-03-20 00:59:18 +0000

    version to 0.9.5 for release
    + enforce code standardizations

--- Templates/RapidXaml.Templates/RapidXamlTemplatesPackage.cs
+++ Templates/RapidXaml.Templates/RapidXamlTemplatesPackage.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.VisualStudio.Shell;
@@ -6,7 +9,7 @@ using Task = System.Threading.Tasks.Task;
 
 namespace RapidXaml.Templates
 {
-    [InstalledProductRegistration("#110", "#112", "0.9.4", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration("#110", "#112", "0.9.5", IconResourceID = 400)] // Info on this package for Help/About
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(RapidXamlTemplatesPackage.PackageGuidString)]
     public sealed class RapidXamlTemplatesPackage : AsyncPackage
