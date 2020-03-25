c6fc6b247bb69c4128766e3c339f3c4af048f207
Matt Lacey <matt@mrlacey.co.uk>
2020-03-25 10:13:09 +0000

    v0.9.1 for RoslynAnalyzers

--- VSIX/RapidXaml.RoslynAnalyzers/RapidXamlRoslynAnalyzersPackage.cs
+++ VSIX/RapidXaml.RoslynAnalyzers/RapidXamlRoslynAnalyzersPackage.cs
@@ -16,7 +16,7 @@ namespace RapidXamlToolkit
     [ProvideAutoLoad(UIContextGuids.SolutionHasSingleProject, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [InstalledProductRegistration("#110", "#112", "0.9.0", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration("#110", "#112", "0.9.1", IconResourceID = 400)] // Info on this package for Help/About
     [Guid(RapidXamlRoslynAnalyzersPackage.PackageGuidString)]
     public sealed class RapidXamlRoslynAnalyzersPackage : AsyncPackage
     {
