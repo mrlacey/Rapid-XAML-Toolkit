c38ba0ef87f532e19e9bd8aeda6b2c801e3412b2
Matt Lacey <matt@mrlacey.co.uk>
2020-05-05 22:10:25 +0100

    fix broken test

---
+++ VSIX/RapidXamlToolkit.Tests/XamlAnalysis/CustomAnalyzers/AnalyzerTestsBase.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using RapidXaml;
+using RapidXamlToolkit.XamlAnalysis;
+
+namespace RapidXamlToolkit.Tests.XamlAnalysis.CustomAnalyzers
+{
+    // TODO: add tests for new XF related CustomAnalyzers
+    public class AnalyzerTestsBase
+    {
+        internal AnalysisActions GetActions<T>(string xaml, ProjectType projectType = ProjectType.Any)
+            where T : ICustomAnalyzer
+        {
+            var sut = (T)Activator.CreateInstance(typeof(T));
+
+            var element = RapidXamlElementExtractor.GetElement(xaml);
+
+            var result = sut.Analyze(element);
+
+            return result;
+        }
+    }
+}
---
+++ VSIX/RapidXamlToolkit.Tests/XamlAnalysis/CustomAnalyzers/LabelAnalyzerTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidXaml;
+using RapidXamlToolkit.XamlAnalysis.CustomAnalysis;
+
+namespace RapidXamlToolkit.Tests.XamlAnalysis.CustomAnalyzers
+{
+    [TestClass]
+    public class LabelAnalyzerTests : AnalyzerTestsBase
+    {
+        [TestMethod]
+        public void HardCoded_Text_Detected()
+        {
+            var xaml = @"<Label Text=""HCValue"" />";
+
+            var actions = this.GetActions<LabelAnalyzer>(xaml, ProjectType.XamarinForms);
+
+            // TODO: ISSUE#163 update when add support for localizing hard-coded strings in Xamarin.Forms.
+            Assert.IsFalse(actions.IsNone);
+            Assert.AreEqual(1, actions.Actions.Count);
+            Assert.AreEqual(ActionType.HighlightWithoutAction, actions.Actions[0].Action);
+            Assert.AreEqual("RXT201", actions.Actions[0].Code);
+        }
+    }
+}
