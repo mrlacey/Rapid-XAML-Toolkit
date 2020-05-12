77f3ef8f24fc302160e1f91bd1ec30f42dbe4f9c
Matt Lacey <matt@mrlacey.co.uk>
2020-05-12 14:45:35 +0100

    fix logic bug

--- VSIX/RapidXaml.Analysis/XamlAnalysis/CustomAnalysis/XamarinForms/XfImageAnalyzer.cs
+++ VSIX/RapidXaml.Analysis/XamlAnalysis/CustomAnalysis/XamarinForms/XfImageAnalyzer.cs
@@ -13,7 +13,7 @@ namespace RapidXamlToolkit.XamlAnalysis.CustomAnalysis
 
         public override AnalysisActions Analyze(RapidXamlElement element, ExtraAnalysisDetails extraDetails)
         {
-            if (extraDetails.TryGet("framework", out ProjectFramework framework)
+            if (!extraDetails.TryGet("framework", out ProjectFramework framework)
              || framework != ProjectFramework.XamarinForms)
             {
                 return AnalysisActions.None;
