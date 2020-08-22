49ab091a94ef243c6342cd551b6dfddac7e84d7f
Matt Lacey <matt@mrlacey.co.uk>
2020-08-22 23:35:18 +0100

    Create initial project

---
+++ VSIX/RapidXaml.ApplyChanges/XamlConverter.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidXaml.ApplyChanges
+{
+    public static class XamlConverter
+    {
+        public static (bool success, List<string> details) ConvertFile(string xamlFilePath, IEnumerable<ICustomAnalyzer> analyzers)
+        {
+            // TODO
+        }
+
+        public static (bool success, List<string> details) ConvertAllFilesInProject(string projectFilePath, IEnumerable<ICustomAnalyzer> analyzers)
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+}
