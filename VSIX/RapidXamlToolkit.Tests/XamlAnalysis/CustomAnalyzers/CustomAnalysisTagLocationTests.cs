c5dc3a293aee1638aee3efb6890656312bbf8699
Matt Lacey <matt@mrlacey.co.uk>
2020-09-21 13:30:52 +0100

    makes multi-line string tests platform independent

--- VSIX/RapidXamlToolkit.Tests/XamlAnalysis/CustomAnalyzers/CustomAnalysisTagLocationTests.cs
+++ VSIX/RapidXamlToolkit.Tests/XamlAnalysis/CustomAnalyzers/CustomAnalysisTagLocationTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
 // Licensed under the MIT license.
 
+using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RapidXaml;
@@ -14,29 +15,31 @@ namespace RapidXamlToolkit.Tests.XamlAnalysis.CustomAnalyzers
     [TestClass]
     public class CustomAnalysisTagLocationTests
     {
+        private static readonly string WinNewLine = "\r\n";
+
         [TestMethod]
         public void TwoPaneViewAnalyzer_TagLocCheck()
         {
-            var xaml = @"<Page
-    x:Class=""App193.MainPage""
-    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
-    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
-    xmlns:local=""using:App193""
-    xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
-    xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
-    mc:Ignorable=""d"">
-
-    <Grid Background=""{ThemeResource ApplicationPageBackgroundThemeBrush}"">
-        <TwoPaneView>
-            <TwoPaneView.Pane1>
-                <TextBlock Text=""Hello World"" />
-            </TwoPaneView.Pane1>
-            <TwoPaneView.Pane2>
-                <TwoPaneView />
-            </TwoPaneView.Pane2>
-        </TwoPaneView>
-    </Grid>
-</Page>";
+            var xaml = "<Page"
+        + WinNewLine + "    x:Class=\"App193.MainPage\""
+        + WinNewLine + "    xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\""
+        + WinNewLine + "    xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\""
+        + WinNewLine + "    xmlns:local=\"using:App193\""
+        + WinNewLine + "    xmlns:d=\"http://schemas.microsoft.com/expression/blend/2008\""
+        + WinNewLine + "    xmlns:mc=\"http://schemas.openxmlformats.org/markup-compatibility/2006\""
+        + WinNewLine + "    mc:Ignorable=\"d\">"
+        + WinNewLine + ""
+        + WinNewLine + "    <Grid Background=\"{ThemeResource ApplicationPageBackgroundThemeBrush}\">"
+        + WinNewLine + "        <TwoPaneView>"
+        + WinNewLine + "            <TwoPaneView.Pane1>"
+        + WinNewLine + "                <TextBlock Text=\"Hello World\" />"
+        + WinNewLine + "            </TwoPaneView.Pane1>"
+        + WinNewLine + "            <TwoPaneView.Pane2>"
+        + WinNewLine + "                <TwoPaneView />"
+        + WinNewLine + "            </TwoPaneView.Pane2>"
+        + WinNewLine + "        </TwoPaneView>"
+        + WinNewLine + "    </Grid>"
+        + WinNewLine + "</Page>";
 
             this.AssertSingleTagAtLocation(xaml, new TwoPaneViewAnalyzer(), 634);
         }
@@ -44,20 +47,20 @@ namespace RapidXamlToolkit.Tests.XamlAnalysis.CustomAnalyzers
         [TestMethod]
         public void UnoIgnorablesAnalyzer_TagLocCheck()
         {
-            var xaml = @"<Page
-    x:Class=""App193.MainPage""
-    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
-    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
-    xmlns:local=""using:App193""
-    xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
-    xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
-    xmlns:xamarin=""http:/uno.ui/xamarin""
-    mc:Ignorable=""d"">
-
-    <Grid Background=""{ThemeResource ApplicationPageBackgroundThemeBrush}"">
-
-    </Grid>
-</Page>";
+            var xaml = "<Page"
+        + WinNewLine + "    x:Class=\"App193.MainPage\""
+        + WinNewLine + "    xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\""
+        + WinNewLine + "    xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\""
+        + WinNewLine + "    xmlns:local=\"using:App193\""
+        + WinNewLine + "    xmlns:d=\"http://schemas.microsoft.com/expression/blend/2008\""
+        + WinNewLine + "    xmlns:mc=\"http://schemas.openxmlformats.org/markup-compatibility/2006\""
+        + WinNewLine + "    xmlns:xamarin=\"http:/uno.ui/xamarin\""
+        + WinNewLine + "    mc:Ignorable=\"d\">"
+        + WinNewLine + ""
+        + WinNewLine + "    < Grid Background=\"{ThemeResource ApplicationPageBackgroundThemeBrush}\">"
+        + WinNewLine + ""
+        + WinNewLine + "    </Grid>"
+        + WinNewLine + "</Page>";
 
             this.AssertSingleTagAtLocation(xaml, new UnoIgnorablesAnalyzer(), 389);
         }
@@ -65,16 +68,16 @@ namespace RapidXamlToolkit.Tests.XamlAnalysis.CustomAnalyzers
         [TestMethod]
         public void BindingToXBindAnalyzer_TagLocCheck()
         {
-            var xaml = @"<Page>
-    <Grid>
-        <TextBox
-            x:Name=""ThisIsADeliberatelyReallyLongName""
-            Margin=""40, 40, 40, 40""
-            Padding=""10, 0, 20, 4""
-            Text=""{Binding SomeValue}""
-            />
-    </Grid>
-</Page>";
+            var xaml = "<Page>"
+        + WinNewLine + "    <Grid>"
+        + WinNewLine + "        <TextBox"
+        + WinNewLine + "            x:Name=\"ThisIsADeliberatelyReallyLongName\""
+        + WinNewLine + "            Margin=\"40, 40, 40, 40\""
+        + WinNewLine + "            Padding=\"10, 0, 20, 4\""
+        + WinNewLine + "            Text=\"{Binding SomeValue}\""
+        + WinNewLine + "            />"
+        + WinNewLine + "    </Grid>"
+        + WinNewLine + "</Page>";
 
             this.AssertSingleTagAtLocation(xaml, new BindingToXBindAnalyzer(), 179);
         }
