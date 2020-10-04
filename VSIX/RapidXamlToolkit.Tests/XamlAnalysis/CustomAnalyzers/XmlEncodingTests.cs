2495eb766276c5d21ca4f178289a4a6b402541fd
Matt Lacey <matt@mrlacey.co.uk>
2020-10-04 20:55:39 +0100

    Add tests for files containing <lt; but unencoded '>'
    For #400

---
+++ VSIX/RapidXamlToolkit.Tests/XamlAnalysis/CustomAnalyzers/XmlEncodingTests.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidXaml;
+using RapidXamlToolkit.XamlAnalysis;
+
+namespace RapidXamlToolkit.Tests.XamlAnalysis.CustomAnalyzers
+{
+    [TestClass]
+    public class XmlEncodingTests
+    {
+        [TestMethod]
+        public void AnalyzerGetsCorrectContent()
+        {
+            var xaml = @"
+<StackPanel>
+    <TextBlock>&lt;Page.Resources>NONE&lt;/Page.Resources></TextBlock>
+</StackPanel>";
+
+            var result = new RapidXamlDocument();
+
+            var snapshot = new FakeTextSnapshot(xaml.Length);
+            var logger = DefaultTestLogger.Create();
+            var vsa = new TestVisualStudioAbstraction();
+
+            var analyzer = new XmlEncodingTestAnalyzer();
+
+            var processors = RapidXamlDocument.WrapCustomProcessors(
+                new List<ICustomAnalyzer> { analyzer },
+                ProjectType.Unknown,
+                string.Empty,
+                logger,
+                vsa);
+
+            XamlElementExtractor.Parse("Generic.xaml", snapshot, xaml, processors.ToList(), result.Tags, null, null, logger);
+
+            Assert.AreEqual("&lt;Page.Resources>NONE&lt;/Page.Resources>", analyzer.Element.Content);
+        }
+
+        public class XmlEncodingTestAnalyzer : ICustomAnalyzer
+        {
+            public RapidXamlElement Element { get; set; }
+
+            public string TargetType() => "TextBlock";
+
+            public AnalysisActions Analyze(RapidXamlElement element, ExtraAnalysisDetails extraDetails)
+            {
+                this.Element = element;
+
+                return AnalysisActions.None;
+            }
+        }
+    }
+}
