32c083eb00cd1f8e187eece2e94eacaa009f68e2
Matt Lacey <matt@mrlacey.co.uk>
2020-10-17 18:36:38 +0100

    add separate LabelProcessor for testing

--- VSIX/RapidXaml.Analysis/XamlAnalysis/Processors/TextBlockProcessor.cs
+++ VSIX/RapidXaml.Analysis/XamlAnalysis/Processors/TextBlockProcessor.cs
@@ -41,4 +41,37 @@ namespace RapidXamlToolkit.XamlAnalysis.Processors
                 this.ProjectType);
         }
     }
+    public class LabelProcessor : XamlElementProcessor
+    {
+        public LabelProcessor(ProcessorEssentials essentials)
+            : base(essentials)
+        {
+        }
+
+        public override void Process(string fileName, int offset, string xamlElement, string linePadding, ITextSnapshot snapshot, TagList tags, List<TagSuppression> suppressions = null, Dictionary<string, string> xlmns = null)
+        {
+            if (!this.ProjectType.Matches(ProjectType.XamarinForms))
+            {
+                return;
+            }
+
+            var (uidExists, uid) = this.GetOrGenerateUid(xamlElement, Attributes.Text);
+
+            this.CheckForHardCodedAttribute(
+                fileName,
+                Elements.TextBlock,
+                Attributes.Text,
+                AttributeType.Any,
+                StringRes.UI_XamlAnalysisHardcodedStringTextblockTextMessage,
+                xamlElement,
+                snapshot,
+                offset,
+                uidExists,
+                uid,
+                Guid.Empty,
+                tags,
+                suppressions,
+                this.ProjectType);
+        }
+    }
 }
