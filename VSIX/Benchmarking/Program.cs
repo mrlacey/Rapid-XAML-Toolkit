b883d675a911f7d71f8e30b3e2f597cd936ae5d4
Matt Lacey <matt@mrlacey.co.uk>
2021-02-12 23:18:44 +0000

    update dependencies
    - address all warnings
    - InternalAnalyzers is now NetStandard2.0
    - Change Benchmarking to be a .NETFramework app (not NetCore)
    - only check for secrets in release build

--- VSIX/Benchmarking/Program.cs
+++ VSIX/Benchmarking/Program.cs
@@ -6,7 +6,9 @@ namespace Benchmarking
     {
         static void Main(string[] _)
         {
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
             var summary = BenchmarkRunner.Run<AnalysisParserBenchmarks>();
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
 
             // For identifying areas for improvement,
             // - Comment out the above call
