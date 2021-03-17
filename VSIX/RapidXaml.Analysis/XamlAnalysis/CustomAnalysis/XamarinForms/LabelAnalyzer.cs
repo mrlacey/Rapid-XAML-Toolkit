1c7879f9931bba9bb96d8f1f91108201a8642944
Matt Lacey <matt@mrlacey.co.uk>
2021-03-17 21:07:02 +0000

    remove old comments

--- VSIX/RapidXaml.Analysis/XamlAnalysis/CustomAnalysis/XamarinForms/LabelAnalyzer.cs
+++ VSIX/RapidXaml.Analysis/XamlAnalysis/CustomAnalysis/XamarinForms/LabelAnalyzer.cs
@@ -17,25 +17,6 @@ namespace RapidXamlToolkit.XamlAnalysis.CustomAnalysis
         public override AnalysisActions Analyze(RapidXamlElement element, ExtraAnalysisDetails extraDetails)
         {
             return this.CheckForHardCodedString(Attributes.Text, AttributeType.Any, element, extraDetails);
-
-            ////var txtAttr = element.GetAttributes(Attributes.Text).FirstOrDefault();
-
-            ////if (txtAttr != null && txtAttr.HasStringValue)
-            ////{
-            ////    var value = txtAttr.StringValue;
-
-            ////    // TODO: ISSUE#163 change this to an RXT200 when can handle localization of Xamarin.Forms apps
-            ////    if (!string.IsNullOrWhiteSpace(value) && char.IsLetterOrDigit(value[0]))
-            ////    {
-            ////        return AnalysisActions.HighlightAttributeWithoutAction(
-            ////        errorType: RapidXamlErrorType.Warning,
-            ////        code: "RXT201",
-            ////        description: StringRes.UI_XamlAnalysisGenericHardCodedStringDescription.WithParams(Elements.Label, Attributes.Text, value),
-            ////        attribute: txtAttr);
-            ////    }
-            ////}
-
-            ////return AnalysisActions.None;
         }
     }
 }
