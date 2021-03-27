7491ba0c9fc4669b7f8743eb3312103c7332ba98
Matt Lacey <matt@mrlacey.co.uk>
2021-03-27 14:58:51 +0000

    add analyzers for hardcoded RadioButton.Text

--- VSIX/RapidXamlToolkit.Tests/AnalysisActionsAssert.cs
+++ VSIX/RapidXamlToolkit.Tests/AnalysisActionsAssert.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
 // Licensed under the MIT license.
 
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RapidXaml;
 
@@ -15,8 +16,10 @@ namespace RapidXamlToolkit.Tests
             Assert.AreEqual(ActionType.CreateResource, actions.Actions[0].Action);
             Assert.AreEqual("RXT200", actions.Actions[0].Code);
             Assert.AreEqual(2, actions.Actions[0].SupplementaryActions.Count);
-            Assert.AreEqual(ActionType.RemoveAttribute, actions.Actions[0].SupplementaryActions[0].Action);
-            Assert.AreEqual(ActionType.AddAttribute, actions.Actions[0].SupplementaryActions[1].Action);
+
+            // Don't worry about the order of these
+            Assert.IsTrue(actions.Actions[0].SupplementaryActions.Any(a => a.Action == ActionType.RemoveAttribute));
+            Assert.IsTrue(actions.Actions[0].SupplementaryActions.Any(a => a.Action == ActionType.AddAttribute));
         }
     }
 }
