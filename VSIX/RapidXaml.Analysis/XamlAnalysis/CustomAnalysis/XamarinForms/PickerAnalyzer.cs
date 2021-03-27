4e418df88728808eab6d99923dea262fe984c2d8
Matt Lacey <matt@mrlacey.co.uk>
2021-03-27 13:19:52 +0000

    handle hardcoded picker title

--- VSIX/RapidXaml.Analysis/XamlAnalysis/CustomAnalysis/XamarinForms/PickerAnalyzer.cs
+++ VSIX/RapidXaml.Analysis/XamlAnalysis/CustomAnalysis/XamarinForms/PickerAnalyzer.cs
@@ -18,24 +18,7 @@ namespace RapidXamlToolkit.XamlAnalysis.CustomAnalysis
 
         public override AnalysisActions Analyze(RapidXamlElement element, ExtraAnalysisDetails extraDetails)
         {
-            var ttlAttr = element.GetAttributes(Attributes.Title).FirstOrDefault();
-
-            if (ttlAttr != null && ttlAttr.HasStringValue)
-            {
-                var value = ttlAttr.StringValue;
-
-                // TODO: ISSUE#163 change this to an RXT200 when can handle localization of Xamarin.Forms apps
-                if (!string.IsNullOrWhiteSpace(value) && char.IsLetterOrDigit(value[0]))
-                {
-                    return AnalysisActions.HighlightAttributeWithoutAction(
-                    errorType: RapidXamlErrorType.Warning,
-                    code: "RXT201",
-                    description: StringRes.UI_XamlAnalysisGenericHardCodedStringDescription.WithParams(Elements.Picker, Attributes.Title, value),
-                    attribute: ttlAttr);
-                }
-            }
-
-            return AnalysisActions.None;
+            return this.CheckForHardCodedString(Attributes.Title, AttributeType.InlineOrElement, element, extraDetails);
         }
     }
 }
