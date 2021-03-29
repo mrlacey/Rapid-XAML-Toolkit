5d82c30ae78e9fb904b8644452194602b0a1a7c2
Matt Lacey <matt@mrlacey.co.uk>
2021-03-29 16:26:08 +0100

    Add AppBarButton.Icon visualizer to EditorExtras

---
+++ VSIX/RapidXaml.EditorExtras/SymbolVisualizer/AppBarButtonTagger.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.Text;
+
+namespace RapidXaml.EditorExtras.SymbolVisualizer
+{
+    internal sealed class AppBarButtonTagger : SymbolIconRegexTagger
+    {
+        internal AppBarButtonTagger(ITextBuffer buffer)
+            : base(buffer, @"(Icon="")([a-z2]{2,})("")")
+        {
+        }
+
+        protected override SymbolIconTag TryCreateTagForMatch(Match match, int lineStart, int spanStart, string snapshotText)
+        {
+            return this.TryCreateSymbolIconTagForMatch(match, lineStart, spanStart, snapshotText, SymbolType.Symbol, "AppBarButton");
+        }
+    }
+}
