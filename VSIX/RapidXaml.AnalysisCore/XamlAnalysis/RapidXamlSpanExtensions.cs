b119f21e4f3b27756553f0788e84936231a1bd13
Matt Lacey <matt@mrlacey.co.uk>
2021-06-01 20:35:32 +0100

    remove unused method with undesirable dependencies

--- VSIX/RapidXaml.AnalysisCore/XamlAnalysis/RapidXamlSpanExtensions.cs
+++ VSIX/RapidXaml.AnalysisCore/XamlAnalysis/RapidXamlSpanExtensions.cs
@@ -1,19 +1,12 @@
 ﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
 // Licensed under the MIT license.
 
-using Microsoft.VisualStudio.Text;
 using RapidXaml;
-using RapidXamlToolkit.VisualStudioIntegration;
 
 namespace RapidXamlToolkit.XamlAnalysis
 {
     public static class RapidXamlSpanExtensions
     {
-        public static Span ToSpanPlusStartPos(this RapidXamlSpan source, int fileStartPos)
-        {
-            return new Span(fileStartPos + source.Start, source.Length);
-        }
-
         public static RapidXamlSpan AddStartPos(this RapidXamlSpan source, int fileStartPos)
         {
             return new RapidXamlSpan(fileStartPos + source.Start, source.Length);
