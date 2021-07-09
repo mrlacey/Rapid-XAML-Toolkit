a4c90a5ba7b25af9d511f2ee6102ae5f129b017a
Matt Lacey <matt@mrlacey.co.uk>
2021-07-09 14:18:13 +0100

    Increase MAUI support

--- VSIX/RapidXaml.Analysis/XamlAnalysis/Processors/TextBlockProcessor.cs
+++ VSIX/RapidXaml.Analysis/XamlAnalysis/Processors/TextBlockProcessor.cs
@@ -17,7 +17,7 @@ namespace RapidXamlToolkit.XamlAnalysis.Processors
 
         public override void Process(string fileName, int offset, string xamlElement, string linePadding, ITextSnapshotAbstraction snapshot, TagList tags, List<TagSuppression> suppressions = null, Dictionary<string, string> xlmns = null)
         {
-            if (this.ProjectType == ProjectType.XamarinForms)
+            if (this.ProjectType == ProjectType.XamarinForms || this.ProjectType == ProjectType.MAUI)
             {
                 return;
             }
