30b63d0b24be82242919bbfcd7d59b5a734fefa7
Hazel Megan <matt@mrlacey.co.uk>
2022-02-09 19:52:25 +0000

    Add more tests for AnalysisExe

---
+++ VSIX/RapidXamlToolkit.Tests.AnalysisExe/CallExeTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RapidXamlToolkit.Tests.AnalysisExe
+{
+    [TestClass]
+    public class CallExeTests
+    {
+        [TestMethod]
+        public void GiveCorrectErrorCodeWhen_NoInputFileProvided()
+        {
+            var (exitCode, _) = RunApp("");
+
+            Assert.AreEqual(1, exitCode);
+        }
+
+        [TestMethod]
+        public void GiveCorrectErrorCodeWhen_InputFileDoesNotExist()
+        {
+            var (exitCode, _) = RunApp(@"..\..\..\BuildAnalysisUwpTestApp\does.not.exist");
+
+            Assert.AreEqual(2, exitCode);
+        }
+
+        [TestMethod]
+        public void GiveCorrectErrorCodeWhen_InputFileIsNotASupportedType()
+        {
+            var (exitCode, _) = RunApp(@"..\..\..\BuildAnalysisUwpTestApp\package.appxmanifest");
+
+            Assert.AreEqual(3, exitCode);
+        }
+
+        [TestMethod]
+        public void TestThatGetOutputFromRealProject()
+        {
+            var args = @"..\..\..\BuildAnalysisUwpTestApp\BuildAnalysisUwpTestApp.csproj";
+
+            var (exitCode, output) = RunApp(args);
+
+            Assert.AreEqual(0, exitCode);
+            Assert.IsTrue(output.Length > 200);
+        }
+
+        private (int exitCode, string output) RunApp(string args)
+        {
+#if DEBUG
+            var relExePath = @"..\..\..\RapidXaml.AnalysisExe\bin\debug\net472\RapidXaml.AnalysisExe.exe";
+#else
+            var relExePath = @"..\..\..\RapidXaml.AnalysisExe\bin\Release\net472\RapidXaml.AnalysisExe.exe";
+#endif
+
+            var startInfo = new ProcessStartInfo(relExePath)
+            {
+                Arguments = args,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+            };
+
+            var process = Process.Start(startInfo);
+
+            var output = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit();
+
+            return (process.ExitCode, output);
+        }
+    }
+}
---
+++ VSIX/RapidXamlToolkit.Tests.AnalysisExe/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RapidXamlToolkit.Tests.AnalysisExe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RapidXamlToolkit.Tests.AnalysisExe")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51a8c2a7-bb31-4cca-b9bd-ad227799be38")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
