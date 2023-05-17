cc1eb31e590928f6a7820117a989d1154d6299d6
Matt Lacey <matt@mrlacey.co.uk>
2023-05-17 10:06:18 +0100

    CustomAnalysis 0.14.3 + symbol gen&signing

---
+++ VSIX/RapidXamlToolkit.Tests/Utils/ColorHelperTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidXaml;
+using RapidXamlToolkit.Tests.XamlAnalysis.CustomAnalyzers;
+using RapidXamlToolkit.XamlAnalysis.Processors;
+
+namespace RapidXamlToolkit.Tests.XamlAnalysis.Processors
+{
+    [TestClass]
+    public class ColorHelperTests
+    {
+        [TestMethod]
+        public void Invalid_HandledCorrectly()
+        {
+            var actual = ColorHelper.GetColor("invalid");
+
+            Assert.IsNull(actual);
+        }
+    }
+}
