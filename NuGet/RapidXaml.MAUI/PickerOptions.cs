42da5729620f43086d83d200ab60aa83f526d404
Matt Lacey <matt@mrlacey.co.uk>
2024-02-06 21:20:02 +0000

    Add MAUI PickerOptions.Csv

---
+++ NuGet/RapidXaml.MAUI/PickerOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace RapidXaml;
+
+public partial class PickerOptions : BindableObject
+{
+    public static readonly BindableProperty CsvProperty = BindableProperty.CreateAttached("Csv", typeof(string), typeof(PickerOptions), string.Empty, propertyChanged: OnCsvChanged);
+
+    private static void OnCsvChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is Picker picker)
+        {
+            if (newValue is string value)
+            {
+                var options = value.Split(',');
+                picker.ItemsSource = options;
+            }
+        }
+    }
+
+    public static string GetCsv(BindableObject view)
+    {
+        return (string)view.GetValue(CsvProperty);
+    }
+
+    public static void SetStrings(BindableObject view, string value)
+    {
+        view.SetValue(CsvProperty, value);
+    }
+}
