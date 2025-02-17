79ff48417f9303b8806babd497969ae5faf3b39e
Matt Lacey <matt@mrlacey.co.uk>
2025-02-17 12:19:57 +0000

    Add first GestureRecognizers

---
+++ NuGet/RapidXaml.MAUI/GestureRecognizer.cs
@@ -0,0 +1,208 @@
+﻿using System.Windows.Input;
+
+namespace RapidXaml.MAUI;
+
+public partial class GestureRecognizer : BindableObject
+{
+	/// <summary>
+	/// Command to execute when the view is tapped a single time.
+	/// </summary>
+	public static readonly BindableProperty SingleTapProperty =
+		BindableProperty.CreateAttached("SingleTap", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSingleTapChanged);
+
+	private static void OnSingleTapChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is TapGestureRecognizer oldTgr && oldTgr.Command == oldCmd)
+					{
+						view.GestureRecognizers.Remove(oldTgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				view.GestureRecognizers.Add(new TapGestureRecognizer { Command = newCmd });
+			}
+		}
+	}
+
+	public static ICommand GetSingleTap(BindableObject view)
+		=> (ICommand)view.GetValue(SingleTapProperty);
+
+	public static void SetSingleTap(BindableObject view, ICommand value)
+		=> view.SetValue(SingleTapProperty, value);
+
+	/// <summary>
+	/// Command to execute when the view is tapped a single time.
+	/// </summary>
+	public static readonly BindableProperty TapProperty =
+		BindableProperty.CreateAttached("Tap", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSingleTapChanged);
+
+	public static ICommand GetTap(BindableObject view)
+		=> (ICommand)view.GetValue(TapProperty);
+
+	public static void SetTap(BindableObject view, ICommand value)
+		=> view.SetValue(TapProperty, value);
+
+	/// <summary>
+	/// Command to execute when the view is tapped twice in quick succession.
+	/// </summary>
+	public static readonly BindableProperty DoubleTapProperty =
+		BindableProperty.CreateAttached("DoubleTap", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnDoubleTapChanged);
+
+	private static void OnDoubleTapChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is TapGestureRecognizer oldTgr && oldTgr.Command == oldCmd)
+					{
+						view.GestureRecognizers.Remove(oldTgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				view.GestureRecognizers.Add(new TapGestureRecognizer { Command = newCmd, NumberOfTapsRequired = 2 });
+			}
+		}
+	}
+
+	public static ICommand GetDoubleTap(BindableObject view)
+		=> (ICommand)view.GetValue(DoubleTapProperty);
+
+	public static void SetDoubleTap(BindableObject view, ICommand value)
+		=> view.SetValue(DoubleTapProperty, value);
+
+	/// <summary>
+	/// Command to execute when the view is tapped with the 'secondary' button. This is usually the right mouse button.
+	/// </summary>
+	public static readonly BindableProperty SecondaryTapProperty =
+	 BindableProperty.CreateAttached("SecondaryTap", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSecondaryTapChanged);
+
+	private static void OnSecondaryTapChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is TapGestureRecognizer oldTgr && oldTgr.Command == oldCmd)
+					{
+						view.GestureRecognizers.Remove(oldTgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				view.GestureRecognizers.Add(new TapGestureRecognizer { Command = newCmd, Buttons = ButtonsMask.Secondary });
+			}
+		}
+	}
+
+	public static ICommand GetSecondaryTap(BindableObject view)
+		=> (ICommand)view.GetValue(SecondaryTapProperty);
+
+	public static void SetSecondaryTap(BindableObject view, ICommand value)
+		=> view.SetValue(SecondaryTapProperty, value);
+
+
+
+	/// <summary>
+	/// Command to execute when the view is panned. The command will receive a <see cref="PanUpdatedEventArgs"/> as a parameter.
+	/// </summary>
+	public static readonly BindableProperty PanProperty =
+	 BindableProperty.CreateAttached("Pan", typeof(Command<PanUpdatedEventArgs>), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnPanChanged);
+
+	private static void OnPanChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is PanGestureRecognizer oldPgr)
+					{
+						view.GestureRecognizers.Remove(oldPgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				var pgr = new PanGestureRecognizer();
+
+				pgr.PanUpdated += (_, e) => { newCmd.Execute(e); };
+
+				view.GestureRecognizers.Add(pgr);
+			}
+		}
+	}
+
+	public static ICommand GetPan(BindableObject view)
+		=> (ICommand)view.GetValue(PanProperty);
+
+	public static void SetPan(BindableObject view, ICommand value)
+		=> view.SetValue(PanProperty, value);
+
+	/// <summary>
+	/// Command to execute when the view has finished being panned. The command will receive a <see cref="PanUpdatedEventArgs"/> as a parameter.
+	/// </summary>
+	public static readonly BindableProperty PanCompletedProperty =
+	 BindableProperty.CreateAttached("PanCompleted", typeof(Command<PanUpdatedEventArgs>), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnPanCompletedChanged);
+
+	private static void OnPanCompletedChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is PanGestureRecognizer oldPgr)
+					{
+						view.GestureRecognizers.Remove(oldPgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				var pgr = new PanGestureRecognizer();
+
+				pgr.PanUpdated += (_, e) => { if (e.StatusType == GestureStatus.Completed) { newCmd.Execute(e); } };
+
+				view.GestureRecognizers.Add(pgr);
+			}
+		}
+	}
+
+	public static ICommand GetPanCompleted(BindableObject view)
+		=> (ICommand)view.GetValue(PanCompletedProperty);
+
+	public static void SetPanCompleted(BindableObject view, ICommand value)
+		=> view.SetValue(PanCompletedProperty, value);
+}
+
+public partial class Gesture : GestureRecognizer
+{
+	// Inherited version with a simpler name
+}
--- NuGet/RapidXaml.MAUI/RapidXaml.MAUI.md
+++ NuGet/RapidXaml.MAUI/RapidXaml.MAUI.md
@@ -2,5 +2,29 @@
 
 Tools to make working with XAML easier in .NET MAUI projects.
 
+## Includes
 
+The following classes are included:
 
+### XamlThickness
+
+A bindable version of `Microsoft.Maui.Thickness`. Use it like a `Thickness` but with the ability to bind properties when creating in XAML.
+
+### GestureRecognizer
+
+A class with the following attached property to make it easier to bind commands to gesture events:
+
+- `SingleTap`
+- `Tap` (The same as SingleTap but with a shorter name)
+- `DoubleTap`
+- `SecondaryTap`
+- `Pan` (Fires when the `PanUpdated`gesture is detected and passes `PanUpdatedEventArgs` to the command)
+- `PanCompleted` (Fires when the pan gesture is completed and passes `PanUpdatedEventArgs` to the command)
+
+Can also be used with the class name `Gesture` (because shorter names are often better.)
+
+### PickerOptions
+
+A class with the following attached properties to make it easier to set the items in a picker:
+
+- `Csv` (Takes a comma delimited list of strings to set as the items.)
