f4234cefc4f257006c3d2d9ff5de0bc1c5c521d2
Matt Lacey <matt@mrlacey.co.uk>
2025-02-22 14:01:48 +0000

    Create EventToCommand.Switch.Toggled.cs

---
+++ NuGet/RapidXaml.MAUI/EventToCommand/EventToCommand.Switch.Toggled.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows.Input;
+
+namespace RapidXaml;
+
+public partial class EventToCommand : BindableObject
+{
+	public static ICommand GetToggled(BindableObject obj)
+		=> (ICommand)obj.GetValue(ToggledProperty);
+
+	public static void SetToggled(BindableObject obj, ICommand value)
+		=> obj.SetValue(ToggledProperty, value);
+
+	public static readonly BindableProperty ToggledProperty =
+		BindableProperty.CreateAttached("Toggled", typeof(ICommand), typeof(EventToCommand), defaultValue: null, propertyChanged: OnToggledChanged);
+
+	private static void OnToggledChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is Switch swtch)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = swtch.Behaviors.Count; i >= 0; i--)
+				{
+					if (swtch.Behaviors[i] is EventToCommandToggledBehavior oldBehavior && oldBehavior.CommandToInvoke == oldCmd)
+					{
+						swtch.Behaviors.Remove(oldBehavior);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				swtch.Behaviors.Add(new EventToCommandToggledBehavior { CommandToInvoke = newCmd });
+			}
+		}
+	}
+
+	internal partial class EventToCommandToggledBehavior : Behavior<Switch>
+	{
+		public ICommand? CommandToInvoke { get; set; }
+
+		protected override void OnAttachedTo(Switch btn)
+		{
+			btn.Toggled += OnSwitchToggled;
+			base.OnAttachedTo(btn);
+		}
+
+		protected override void OnDetachingFrom(Switch btn)
+		{
+			btn.Toggled -= OnSwitchToggled;
+			base.OnDetachingFrom(btn);
+		}
+
+		private void OnSwitchToggled(object? sender, EventArgs e)
+		{
+			if (this.CommandToInvoke != null)
+			{
+				if (this.CommandToInvoke.CanExecute(null))
+				{
+					this.CommandToInvoke.Execute(null);
+				}
+			}
+		}
+	}
+}
