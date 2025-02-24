23cc73aba4109c21651afbcca0668f2126d0c935
Matt Lacey <matt@mrlacey.co.uk>
2025-02-24 17:27:40 +0000

    Gestures: Swipe, SwipeLeft, SwipeRight, SwipeUp, SwipeDown

--- NuGet/RapidXaml.MAUI/GestureRecognizer.cs
+++ NuGet/RapidXaml.MAUI/GestureRecognizer.cs
@@ -122,7 +122,6 @@ public partial class GestureRecognizer : BindableObject
 		=> view.SetValue(SecondaryTapProperty, value);
 
 
-
 	/// <summary>
 	/// Command to execute when the view is panned. The command will receive a <see cref="PanUpdatedEventArgs"/> as a parameter.
 	/// </summary>
@@ -200,6 +199,207 @@ public partial class GestureRecognizer : BindableObject
 
 	public static void SetPanCompleted(BindableObject view, ICommand value)
 		=> view.SetValue(PanCompletedProperty, value);
+
+
+	/// <summary>
+	/// Command to execute when the view has been swiped.  The command will receive a <see cref="SwipeDirection"/> as a parameter.
+	/// </summary>
+	public static readonly BindableProperty SwipeProperty =
+	 BindableProperty.CreateAttached("Swipe", typeof(Command<SwipeDirection>), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSwipeChanged);
+
+	private static void OnSwipeChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is Command<SwipeDirection> oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is SwipeGestureRecognizer oldSgr)
+					{
+						view.GestureRecognizers.Remove(oldSgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is Command<SwipeDirection> newCmd)
+			{
+				var sgr = new SwipeGestureRecognizer() { Direction = SwipeDirection.Right | SwipeDirection.Left | SwipeDirection.Up | SwipeDirection.Down };
+
+				sgr.Swiped += (_, e) => { newCmd.Execute(e.Direction); };
+
+				view.GestureRecognizers.Add(sgr);
+			}
+		}
+	}
+
+	public static Command<SwipeDirection> GetSwipe(BindableObject view)
+		=> (Command<SwipeDirection>)view.GetValue(SwipeProperty);
+
+	public static void SetSwipe(BindableObject view, Command<SwipeDirection> value)
+		=> view.SetValue(SwipeProperty, value);
+
+
+	/// <summary>
+	/// Command to execute when the view has been swiped to the Left.
+	/// </summary>
+	public static readonly BindableProperty SwipeLeftProperty =
+	 BindableProperty.CreateAttached("SwipeLeft", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSwipeLeftChanged);
+
+	private static void OnSwipeLeftChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is SwipeGestureRecognizer oldSgr)
+					{
+						view.GestureRecognizers.Remove(oldSgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				var sgr = new SwipeGestureRecognizer() { Direction = SwipeDirection.Left };
+
+				sgr.Swiped += (_, e) => { if (e.Direction == SwipeDirection.Left) { newCmd.Execute(e); } };
+
+				view.GestureRecognizers.Add(sgr);
+			}
+		}
+	}
+
+	public static ICommand GetSwipeLeft(BindableObject view)
+		=> (ICommand)view.GetValue(SwipeLeftProperty);
+
+	public static void SetSwipeLeft(BindableObject view, ICommand value)
+		=> view.SetValue(SwipeLeftProperty, value);
+
+
+	/// <summary>
+	/// Command to execute when the view has been swiped to the Right.
+	/// </summary>
+	public static readonly BindableProperty SwipeRightProperty =
+	 BindableProperty.CreateAttached("SwipeRight", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSwipeRightChanged);
+
+	private static void OnSwipeRightChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is SwipeGestureRecognizer oldSgr)
+					{
+						view.GestureRecognizers.Remove(oldSgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				var sgr = new SwipeGestureRecognizer() { Direction = SwipeDirection.Right };
+
+				sgr.Swiped += (_, e) => { if (e.Direction == SwipeDirection.Right) { newCmd.Execute(e); } };
+
+				view.GestureRecognizers.Add(sgr);
+			}
+		}
+	}
+
+	public static ICommand GetSwipeRight(BindableObject view)
+		=> (ICommand)view.GetValue(SwipeRightProperty);
+
+	public static void SetSwipeRight(BindableObject view, ICommand value)
+		=> view.SetValue(SwipeRightProperty, value);
+
+
+	/// <summary>
+	/// Command to execute when the view has been swiped Up.
+	/// </summary>
+	public static readonly BindableProperty SwipeUpProperty =
+	 BindableProperty.CreateAttached("SwipeUp", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSwipeUpChanged);
+
+	private static void OnSwipeUpChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is SwipeGestureRecognizer oldSgr)
+					{
+						view.GestureRecognizers.Remove(oldSgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				var sgr = new SwipeGestureRecognizer() { Direction = SwipeDirection.Up };
+
+				sgr.Swiped += (_, e) => { if (e.Direction == SwipeDirection.Up) { newCmd.Execute(e); } };
+
+				view.GestureRecognizers.Add(sgr);
+			}
+		}
+	}
+
+	public static ICommand GetSwipeUp(BindableObject view)
+		=> (ICommand)view.GetValue(SwipeUpProperty);
+
+	public static void SetSwipeUp(BindableObject view, ICommand value)
+		=> view.SetValue(SwipeUpProperty, value);
+
+
+	/// <summary>
+	/// Command to execute when the view has been swiped Down.
+	/// </summary>
+	public static readonly BindableProperty SwipeDownProperty =
+	 BindableProperty.CreateAttached("SwipeDown", typeof(ICommand), typeof(GestureRecognizer), defaultValue: null, propertyChanged: OnSwipeDownChanged);
+
+	private static void OnSwipeDownChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is View view)
+		{
+			if (oldValue is ICommand oldCmd)
+			{
+				for (int i = view.GestureRecognizers.Count; i >= 0; i--)
+				{
+					if (view.GestureRecognizers[i] is SwipeGestureRecognizer oldSgr)
+					{
+						view.GestureRecognizers.Remove(oldSgr);
+						break;
+					}
+				}
+			}
+
+			if (newValue is ICommand newCmd)
+			{
+				var sgr = new SwipeGestureRecognizer() { Direction = SwipeDirection.Down };
+
+				sgr.Swiped += (_, e) => { if (e.Direction == SwipeDirection.Down) { newCmd.Execute(e); } };
+
+				view.GestureRecognizers.Add(sgr);
+			}
+		}
+	}
+
+	public static ICommand GetSwipeDown(BindableObject view)
+		=> (ICommand)view.GetValue(SwipeDownProperty);
+
+	public static void SetSwipeDown(BindableObject view, ICommand value)
+		=> view.SetValue(SwipeDownProperty, value);
+
 }
 
 public partial class Gesture : GestureRecognizer
